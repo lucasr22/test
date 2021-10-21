362edf8d13a3da6508d234c20f6b15bef447c497
lucasr22 <91099098+lucasr22@users.noreply.github.com>
2021-10-20 21:28:16 -0300

    Add files via upload

---
+++ projetofinal/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sistema de certificado</title>
+</head>
+<body>
+    <nav>
+       <a href="#">home </a>
+</nav>
+    <form action="" method="POST"> 
+        <label for="Email:">Email:</label>
+        <input type="email" name="email">
+        <label for="Senha">Senha:</label>
+        <input type="password" name="senha">  
+        <button type="submit" name="enviar">enviar</button>  
+</form>
+</body>
+</html>
